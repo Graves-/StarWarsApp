aad3f084db2746836f2eb919363095d09d2eef7e
graves- <guillermoortizitc@outlook.com>
2016-10-28 13:32:01 -0500

    Added server configuration on app.js to run server on port 3000.

--- app.js
+++ app.js
@@ -2,3 +2,27 @@ var express = require('express');
 var exphbs  = require('express-handlebars');
 var http = require("http");
 var request = require('request');
+
+var app = express();
+var router = express.Router();
+
+//EXPRESS
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+//SERVE STATIC CONTENT
+app.use(express.static('public'));
+
+//RUTA PARA LA RAIZ DE LA APP / INDEX
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.use("/",router);
+
+app.listen(3000,function(){
+  console.log("Live at Port 3000");
+});
+
+
